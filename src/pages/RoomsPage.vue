640d81f500e32f9243a584863e89d47457f3ea12
FryMondo <ptachyksergei@gmail.com>
2023-11-29 17:40:01 +0200

    Page with rooms in our hotel

---
+++ src/pages/RoomsPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <table id="data-table">
+    <thead>
+    <tr>
+      <th>Номер кімнати</th>
+      <th>Кількість кімнат</th>
+      <th>Площа номеру</th>
+      <th>Кількість місць в номері</th>
+      <th>Тип номеру</th>
+      <th>Ціна кімнати</th>
+    </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+table {
+  width: 60%;
+  margin-left: 100px;
+  padding: 30px;
+  min-height: 90px;
+  border-radius: 10px;
+  border-color: #fff;
+}
+
+th {
+  border-radius: 10px;
+  border: 2px solid #fff;
+  height: 40px;
+
+}
+</style>
