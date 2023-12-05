6b46fb8ac1363554edd87e4c8b8358d404b41eda
FryMondo <ptachyksergei@gmail.com>
2023-12-06 00:45:36 +0200

    Connected tables "roominformation" & "rooms" to the RoomsPage

--- src/server/server.js
+++ src/server/server.js
@@ -71,6 +71,51 @@ app.post('/login', async (req, res) => {
     });
 });
 
+app.post('/reservation', async (req, res) => {
+    const { roomNumber, numberOfRooms, roomArea, placesInRoom, description, roomType, roomCost } = req.body;
+    const ID = 1;
+
+    try {
+        const sqlRoomInfo = 'INSERT INTO roominformation (placesInRoom, roomType, roomCost, ID) VALUES (?, ?, ?, ?)';
+        const roomInformationID = [placesInRoom, roomType, roomCost, ID];
+
+        const sqlRooms = 'INSERT INTO rooms (roomNumber, numberOfRooms, roomArea, description, roomInformationID) VALUES (?, ?, ?, ?, ?)';
+        const RoomsValues = [roomNumber, numberOfRooms, roomArea, description, ID];
+
+        db.query(sqlRoomInfo, roomInformationID, (err, result) => {
+            if (err) {
+                console.log('Error executing query:', err);
+                res.status(500).send('Error adding info into table roominformation');
+                return;
+            }
+
+            db.query(sqlRooms, RoomsValues, (err, result) => {
+                if (err) {
+                    console.log('Error executing query:', err);
+                    res.status(500).send('Error adding info into table rooms');
+                    return;
+                }
+
+                res.status(200).send('Successfully added information in roominformation and rooms');
+            });
+        });
+    } catch (error) {
+        res.status(500).send('Error adding info');
+    }
+});
+
+app.get('/getRooms', (req, res) => {
+    const sql = 'SELECT * FROM rooms INNER JOIN roominformation ON rooms.roomInformationID = roominformation.ID';
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.log('Error executing query:', err);
+            res.status(500).send('Error getting room information');
+            return;
+        }
+        res.status(200).json(results);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
