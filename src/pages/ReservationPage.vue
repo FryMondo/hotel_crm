361beefa320625c75656dc7d3526f7812013af67
FryMondo <ptachyksergei@gmail.com>
2023-12-13 21:08:03 +0200

    Adding info from fields to DB

--- src/pages/ReservationPage.vue
+++ src/pages/ReservationPage.vue
@@ -1,31 +1,73 @@
 <template>
   <body>
   <div class="form-box">
-    <form>
+    <form @submit.prevent="reserveRoom">
       <h2>Бронювання номеру</h2>
       <div class="input-box">
-        <input type="date" required>
+        <input v-model="startDate" type="date">
         <label>Початок бронювання:</label>
       </div>
       <div class="input-box">
-        <input type="date" required>
+        <input v-model="endDate" type="date">
         <label>Кінець бронювання:</label>
       </div>
       <div class="input-box">
-        <input type="number" required>
+        <input v-model="roomNumber" type="number">
         <label>Номер кімнати:</label>
       </div>
       <div class="input-box">
-        <input type="number" required>
+        <input v-model="numberOfPeople" type="number">
         <label>Кількість людей в кімнаті:</label>
       </div>
-      <button @click="$router.push('/')">Забронювати</button>
+      <button type="submit">Забронювати</button>
+      <button @click="$router.push('/')">Повернутися на головну сторінку</button>
     </form>
   </div>
   </body>
 </template>
 <script>
-export default {}
+export default {
+  data(){
+    return {
+      startDate: '',
+      endDate: '',
+      roomNumber: '',
+      numberOfPeople: ''
+    }
+  },
+  methods: {
+    async reserveRoom() {
+      try {
+        const username = localStorage.getItem('username');
+        if (!username) {
+          return;
+        }
+
+        const responseAddInfo = await fetch('http://localhost:3000/reserveRoom', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            usernameID: username,
+            startDate: this.startDate,
+            endDate: this.endDate,
+            roomNumberID: this.roomNumber,
+            numberOfPeople: this.numberOfPeople
+          }),
+        });
+
+        if (responseAddInfo.ok) {
+          console.log("Information added successfully!");
+        } else {
+          console.error('Error:', responseAddInfo.statusText);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -46,6 +88,7 @@ body {
   border: 2px solid rgba(255, 255, 255, 0.5);
   border-radius: 20px;
   backdrop-filter: blur(10px);
+  width: 13%;
 }
 
 h2 {
@@ -97,5 +140,6 @@ button {
   cursor: pointer;
   font-size: 1em;
   font-weight: 600;
+  margin: 5px;
 }
 </style>
