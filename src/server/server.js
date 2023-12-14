bbea6f40fdcae6897ec6934ce64fee5f2f3b0000
FryMondo <ptachyksergei@gmail.com>
2023-12-15 01:35:21 +0200

    Getting role from "roles"

--- src/server/server.js
+++ src/server/server.js
@@ -185,6 +185,18 @@ app.get('/getAvailableRooms', (req, res) => {
     });
 });
 
+app.get('/getUserRole', (req, res) => {
+    const { username } = req.query;
+    const sql = 'SELECT role FROM roles WHERE username = ?';
+    db.query(sql, [username], (err, results) => {
+        if (err) {
+            console.log('Error executing query:', err);
+            res.status(500).send('Error getting role');
+            return;
+        }
+        res.status(200).json(results);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
