3e3ccc536f2320b331c00c1a0a24a3c71eb106dd
FryMondo <ptachyksergei@gmail.com>
2023-12-15 10:33:54 +0200

    Adding userinfo

--- src/server/serverParts/AddUserInfoServer.js
+++ src/server/serverParts/AddUserInfoServer.js
@@ -1,19 +1,17 @@
 const db = require('../database');
 
 module.exports = (req, res) => {
-    const {username, phone, surname, firstName} = req.body;
-    try {
-        const sqlUserInfo = 'INSERT INTO userinfo (username, phone, surname, firstName) VALUES (?, ?, ?, ?)';
-        const valuesUserInfo = [username, phone, surname, firstName];
-        db.query(sqlUserInfo, valuesUserInfo, (err, result) => {
-            if (err) {
-                console.log('Error executing query:', err);
-                res.status(500).send('Error adding info');
-                return;
-            }
-            res.status(200).send('Information added successfully');
-        });
-    } catch (error) {
-        res.status(500).send('Error adding info');
-    }
+    const { username, phone, surname, firstName } = req.body;
+    const sql = 'INSERT INTO userinfo (username, phone, surname, firstName) VALUES (?, ?, ?, ?)';
+    const values = [username, phone, surname, firstName];
+
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            console.log('Error executing query:', err);
+            res.status(500).send('Error adding user info');
+            return;
+        }
+
+        res.status(200).send('User info added successfully');
+    });
 };
