bfc6b1a4ebaa2b62244bbb6a484ceabb2b15f1ea
FryMondo <ptachyksergei@gmail.com>
2023-12-17 00:27:48 +0200

    Take info from the table "roomreservation"

--- src/server/serverParts/Get_reserved.js
+++ src/server/serverParts/Get_reserved.js
@@ -1,15 +1,14 @@
 const db = require('../database');
 
 module.exports = (req, res) => {
-    const {username} = req.query;
-    const sql = 'SELECT * FROM roomreservation FOR username';
-    db.query(sql, (err, results) => {
+    const {usernameID} = req.query;
+    const sql = 'SELECT startDate, endDate, roomNumberID, numberOfPeople FROM roomreservation WHERE usernameID = ?';
+    db.query(sql, [usernameID], (err, results) => {
         if (err) {
             console.log('Error executing query:', err);
-            res.status(500).send('Error getting room information');
+            res.status(500).send('Error getting role');
             return;
         }
         res.status(200).json(results);
     });
-
 };
