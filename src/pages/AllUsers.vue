c3fe1daa1c67dbdcbfb81b2dbdd4d7f8c7edaee0
FryMondo <ptachyksergei@gmail.com>
2023-12-17 13:38:14 +0200

    Page with all users

---
+++ src/pages/AllUsers.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="main-btn">
+    <button @click="$router.push('/')">На головну сторінку</button>
+  </div>
+  <table id="data-table">
+    <thead>
+    <tr>
+      <th>Email</th>
+      <th>Username</th>
+      <th>Роль</th>
+      <th>Телефон</th>
+      <th>Прізвище</th>
+      <th>Ім'я</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="user in users" :key="user.username">
+      <td>{{ user.email }}</td>
+      <td>{{ user.username }}</td>
+      <td>{{ user.role }}</td>
+      <td>{{ user.phone || 'Порожньо' }}</td>
+      <td>{{ user.surname || 'Порожньо' }}</td>
+      <td>{{ user.firstName || 'Порожньо' }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      users: []
+    }
+  },
+  methods: {
+    async fetchAllUsers() {
+      const username = localStorage.getItem('username');
+      try {
+        const response = await fetch(`http://localhost:3000/getAllUsers?username=${username}`);
+        if (response.ok) {
+          this.users = await response.json();
+        } else {
+          console.error('Server response not OK');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchAllUsers()
+  }
+}
+</script>
+
+<style scoped src="./styles/AllUsersStyles.css"></style>
